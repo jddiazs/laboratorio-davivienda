4e964c7d595bb4ae5ffc124bae25ff1f425a123e
juliandiaz <julian.diaz@itac.co>
2026-04-09 15:51:32 -0500

    feat((DVLAB-1) - HU(DVLAB-1)): :wrench: install and configure primeng primeflex primeicons

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(),
+    ConfirmationService,
+    MessageService
+  ]
 };
